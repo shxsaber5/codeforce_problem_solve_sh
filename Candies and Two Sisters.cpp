f656a7456d417abd148ed29bc36a7200cb93455d
Saber Hossain <shsaber500@gmail.com>
2025-07-20 23:30:16 +0600

    Candies and Two Sisters.cpp

---
+++ Candies and Two Sisters.cpp
@@ -0,0 +1,16 @@
+// Problem Link:-https://codeforces.com/problemset/problem/1335/A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int t;cin>>t;
+    while (t--)
+    {
+        long long n;cin>>n;
+        cout <<(n-1)/2 << endl;
+    }
+    
+  
+ 
+ return 0;
+}
