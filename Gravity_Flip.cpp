211ceaf2fcae968c2195d129a46b6fe5b5c0d9ba
Saber Hossain <shsaber500@gmail.com>
2025-07-30 22:27:37 +0600

    Gravity_Flip.cpp

---
+++ Gravity_Flip.cpp
@@ -0,0 +1,23 @@
+// Problem Link:-https://codeforces.com/contest/405/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    
+    vector<int> v(n);
+    
+    for(int i=0; i<n; i++) 
+        cin>>v[i];
+        
+    sort(v.begin(), v.end());
+    
+    for(int i=0; i<v.size(); i++) 
+        cout<<v[i]<<" ";
+        
+    cout<<endl;
+    
+    return 0;
+}
