aec84e0a4cb0d93bc80bad587711abb2732ad6b1
Saber Hossain <shsaber500@gmail.com>
2025-07-31 22:59:08 +0600

    Domino piling.cpp

---
+++ Domino piling.cpp
@@ -0,0 +1,16 @@
+// Problem Link:-https://codeforces.com/contest/50/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m,n,square;
+    cin >> m >> n;
+    
+    square = m*n;
+    
+    int result = square/2;
+    
+    cout << result << endl;
+    return 0;
+}
