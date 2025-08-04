85015992f1c8e8391a1d6ba360090c83d1044dee
Saber Hossain <shsaber500@gmail.com>
2025-08-04 22:50:00 +0600

    In Search of an Easy Problem.cpp

---
+++ In Search of an Easy Problem.cpp
@@ -0,0 +1,21 @@
+// Problem Link:-https://codeforces.com/contest/1030/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++) 
+        cin>>arr[i];
+ 
+    for(int i=0; i<n; i++){
+        if(arr[i]==1){
+            cout<<"HARD"<<endl;
+            return 0;
+        }
+    }
+    cout<<"EASY"<<endl;
+    return 0;
+}
